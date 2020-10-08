22bbed2d7f106030b0507332652a6f3a7c34c8ef
gabrielweb3 <golivera.gr@gmail.com>
2020-10-07 23:46:28 -0300

    comiteado 7/10

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,7 @@
 
 //FUNCION PARA MOSTRAR ARTICULOS DEL CARRITO
 var articulo = [];
-function mostrar_en_pantalla(array){
+function mostrar_en_pantalla(array,cantidad_input){
 	//variables html para imprimir carrito y factura
 	let html = '';
 	let html_factura = '';
@@ -10,7 +10,6 @@ function mostrar_en_pantalla(array){
 	var FACTURA_total = 0;
 	var FACTURA_sub_total = 0;
 	var CLIENTE = localStorage.getItem("user");
-	//var ENVIO_valor = 0;
 	var ENVIO_tipo = "";
 	var multiplicador = "";
 
@@ -18,10 +17,11 @@ function mostrar_en_pantalla(array){
 	var ENVIO_gold = document.getElementById("goldradio").checked;
 	var ENVIO_premium = document.getElementById("premiumradio").checked;
 	var ENVIO_standar = document.getElementById("standardradio").checked;
-	
+
+	//dependiendo el radibutton que está apretado elijo el tipo de envío y porcentaje que corresponde
 	if (ENVIO_gold){
-		multiplicador = 0.13;
-		ENVIO_tipo = "Gold(13%)";
+		multiplicador = 0.15;
+		ENVIO_tipo = "Gold(15%)";
 	}if (ENVIO_premium){
 		multiplicador = 0.07;
 		ENVIO_tipo = "Premium(7%)";
@@ -35,18 +35,20 @@ function mostrar_en_pantalla(array){
 		//valores de los productos para mostrar en el carrito
 		//var no_grabado = "";
 		var unitario = "";
-		var total_dolares = "";
+		var total_dolares = "";		
+		var cantidad_producto = producto.count; //creo una variable unicamente para la cantidad, ya que puede cambiar su valor
 
+		//hago un filtro para la moneda, que tome el valor de los productos en dólares
 		if (producto.currency === "USD"){
-			FACTURA_total = (FACTURA_total + (producto.unitCost*producto.count));
+			FACTURA_total = (FACTURA_total + (producto.unitCost*cantidad_producto));
 			//no_grabado = producto.unitCost/1.22;
 			unitario = producto.unitCost;
-			total_dolares = producto.unitCost*producto.count;
+			total_dolares = producto.unitCost*cantidad_producto;
 		}else{
-			FACTURA_total = (FACTURA_total + ((producto.unitCost/40)*producto.count));
+			FACTURA_total = (FACTURA_total + ((producto.unitCost/40)*cantidad_producto));
 			//no_grabado = (producto.unitCost/40)/1.22;
 			unitario = producto.unitCost/40;
-			total_dolares = (producto.unitCost/40)*producto.count;
+			total_dolares = (producto.unitCost/40)*cantidad_producto;
 		}
 
 		//SUB TOTAL
@@ -61,11 +63,13 @@ function mostrar_en_pantalla(array){
 						</div>
 						<div class="col-xs-6">
 							<div class="col-xs-6 text-right">
-								<h6><strong>Costo unitario:</strong><br> ${unitario.toFixed(2)} U$S</h6>
-								<h6><strong>TOTAL:</strong><br> ${total_dolares.toFixed(2)} U$S</h6>							
+								<h6 id="costo_unitario"><strong>Costo unitario:</strong><br> ${unitario.toFixed(2)} U$S</h6>
+								<h6 id="costo_total"><strong>TOTAL:</strong><br> ${total_dolares.toFixed(2)} U$S</h6>							
 							</div>
 							<div class="col-xs-4">
-							<strong>Cantidad:</strong> <input id="cantidad" type="text" class="form-control input-sm" value="${producto.count}">
+								<button id="add_${i}" onclick="aumentar_cantidad(${i});" style="font-size: smaller;" type="button" class="btn" data-dismiss="modal"><i class="fas fa-plus"></i></button>
+								<button id="quit_${i}" onclick="disminuir_cantidad();" style="font-size: smaller;" type="button" class="btn" data-dismiss="modal"><i class="fas fa-minus"></i></button>
+								<strong>Cantidad:</strong> <input id="cantidad${i}" type="number" class="form-control input-sm" value="${cantidad_producto}" min="0">
 							</div>
 							<div class="col-xs-2">
 								<button type="button" class="btn btn-link btn-xs">
@@ -78,10 +82,11 @@ function mostrar_en_pantalla(array){
             `
 			document.getElementById('lista').innerHTML += html;
 			
+		//escribo los valores en la factura, desglosando cada costo por separado
 		html_factura = `
 			<br>
 				<p><strong> CLIENTE:</strong> ${CLIENTE}</p>				
-				<p><strong> ENVIO:</strong> ${ENVIO_tipo} - ${FACTURA_total*multiplicador} U$S</p>
+				<p><strong> ENVIO:</strong> ${ENVIO_tipo} - ${(FACTURA_total*multiplicador).toFixed(2)} U$S</p>
 				<p><strong> SUB TOTAL:</strong> ${FACTURA_sub_total.toFixed(2)} U$S</p>
 				<p><strong> IVA</strong>(22%)<strong>:</strong> ${(FACTURA_total+(FACTURA_total*multiplicador)-FACTURA_sub_total).toFixed(2)} U$S</p>
 				<p class="renglon" id="Total"><strong> TOTAL:</strong> ${FACTURA_total+(FACTURA_total*multiplicador)} U$S</p><br>
@@ -89,6 +94,19 @@ function mostrar_en_pantalla(array){
 		`
 		document.getElementById("elementos_factura").innerHTML = html_factura;
 	}
+}
+//funcion que limpia la pantalla antes de volver a calcular los valores de producto y envío cuando se selecciona un tipo de envío
+function limpiar_pantalla() {
+	html = ``
+	document.getElementById('lista').innerHTML = html;
+}
+//funcion que aumenta la cantidad modifica la cantidad de producto cuando se apreta el boton +
+function aumentar_cantidad(indice){
+	var cantidades = [];
+	cantidades.push(document.getElementById("cantidad0").value);
+	cantidades.push(document.getElementById("cantidad1").value);
+	alert(cantidades);
+	const boton_apretado = document.getElementById("add_${indice}");
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -101,10 +119,36 @@ document.addEventListener("DOMContentLoaded", function(e){
             informacion_carrito = resultado.data;
             mostrar_en_pantalla(informacion_carrito);
         }
-    })
+	})
+	document.getElementById("premiumradio").addEventListener("click", function(){
+		limpiar_pantalla();
+		mostrar_en_pantalla(informacion_carrito);
+	})
+	document.getElementById("standardradio").addEventListener("click", function(){
+		limpiar_pantalla();
+		mostrar_en_pantalla(informacion_carrito);
+	})
+	document.getElementById("goldradio").addEventListener("click", function(){
+		limpiar_pantalla();
+		mostrar_en_pantalla(informacion_carrito);
+	})
 });
 //CART_INFO_URL
 //"https://japdevdep.github.io/ecommerce-api/cart/654.json"
-
 //{"articles": [{"name": "Pino de olor para el auto","count": 2,"unitCost": 100,"currency": "UYU", "src": "img/tree1.jpg"},
 //{"name": "Suzuki Celerio","count": 1,"unitCost": 12500,"currency": "USD","src": "img/prod3.jpg"}]}
+//<button id="add_" style="font-size: smaller;" type="button" class="btn" data-dismiss="modal"><i class="fas fa-plus"></i></button><button id="quit_" style="font-size: smaller;" type="button" class="btn" data-dismiss="modal"><i class="fas fa-minus"></i></button>
+//<strong>Cantidad:</strong> <input id="cantidad${i}" onchange="calcular(${i});" type="number" class="form-control input-sm" value="${cantidad_producto}" min="0">
+//function calcular(cantidad_input) {
+// 	var cantidades = [];
+// 	cantidades.push(document.getElementById("cantidad0").value);
+// 	cantidades.push(document.getElementById("cantidad1").value);
+
+// 	alert(cantidades);
+// 	html = "";
+// 	for (let i = 0; i < cantidades.length; i++) {
+// 		const cantidad = cantidades[i];
+
+		
+// 	}
+// }
